551d9caaca7f8ae8ec261e4f75fe1d401d5f314e
Stivi <sstev3n@gmail.com>
2013-06-02 19:57:47 +0400

    Преобразовывает в jpg

---
+++ decode.py
@@ -0,0 +1,19 @@
+﻿# -*- coding: utf-8 -*-
+
+import sys, os, base64
+
+inputfile = open('input', 'r')
+outfile = open('out.jpg', 'wb')
+
+base64.decode(inputfile, outfile)
+
+inputfile.close()
+outfile.close()
+
+## Для  полной   уверенности  в  закрытии
+## файла можно использовать блок try/finally:
+## 
+##   try:
+##      # Тут идет запись в файл
+##   finally:
+##      file.close()
