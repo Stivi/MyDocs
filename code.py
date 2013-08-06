584b7fbd476dde993f062732ed813d1880b0847d
Stivi <sstev3n@gmail.com>
2013-08-06 22:39:42 +0400

    Окончательный вариант

--- code.py
+++ code.py
@@ -1,33 +1,47 @@
-﻿>>> import sys, os, codecs
->>> os.chdir('C:\\test')
->>> f = codecs.open("mydict.txt", "r", "utf-8")
+﻿import sys, os, codecs
+#os.chdir('C:\\test')
+f = codecs.open("ref.txt", "r", "utf-8")
+input_text = codecs.open("text.tex", "r", "utf-8")
+out_text = codecs.open("outme", "wb", "utf-8")
 
->>> def pretty(dict):
-	print '-'*60
-	print repr(dict).decode("unicode-escape")
-	print '-'*60
+###def pretty(dict):
+###     #print '-'*60
+###     print repr(dict).decode("unicode-escape")
+###     #print '-'*60
 
->>> def cut_key_to_dict():
-	# переходим в начало файла и обнуляем словарь
-	f.seek(0)
-	my_example_dict.clear()
-	while 1:
-		line = f.readline()
-		if not line:
-			print 'выхожу из цикла'
-			break
-		if line.find('[') == -1:
-			print '1-й цикл - начало'
-			print 'Ключ не найден. Ожидается строка с ключем в начале.'
-			print 'Тут ожидается пустая строка', "|" , line, "|"
-			print '1-й цикл - конец'
-		else:
-			print '2-й цикл - начало'
-			start_key = line.find('[')
-			end_key = line.find(']') + 1
-			key = line[start_key:end_key]
-			print 'Сброс в словарь готового элемента'
-			my_example_dict[key] = line
-			pretty(my_example_dict)
-			print '2-й цикл - конец'
+def replace_all(text, dic):
+    for i, j in dic.iteritems():
+        text = text.replace(i, j)
+    return text
 
+my_example_dict = {}
+
+def pull_to_dict():
+        # переходим в начало файла и обнуляем словарь
+        f.seek(0)
+        my_example_dict.clear()
+        while True:
+                line = f.readline()
+                if not line:
+                        print 'выхожу из цикла'
+                        break
+                if line.find('[') == -1:
+                        pass
+                else:
+                        start_key = line.find('[')
+                        end_key = line.find(']') + 1
+                        key = line[start_key:end_key]
+                        my_example_dict[key] = u'\\footnote{' + line.replace(str(key), '').strip() + u'}'
+
+def push_to_file():
+        out_text.seek(0)
+        while True:
+                inputline = input_text.readline()
+                if inputline:
+                        inputline = replace_all(inputline, my_example_dict)
+                        out_text.write(inputline)
+                else:
+                        print 'выхожу из цикла'
+                        out_text.close()
+                        input_text.close()
+                        break
