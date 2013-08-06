bf2622166361b7534e995877d1f838f55070eb98
sstev3n <sstev3n@gmail.com>
2013-08-06 14:42:11 +0400

    Convert to function

--- code.py
+++ code.py
@@ -12,22 +12,25 @@
 	my_example_dict.clear()
 	my_example_dict['0'] = 'lol'
 
->>> for line in f:
-	# find – находит подстроку в строке – возвращает позицию вхождения строки, либо -1:
-	if line.find('[') == -1:
-		print '1-й цикл - начало'
-		print 'Ключ не найден. Добавляю в словарь предыдущего элемента'
-		print line
-		print '1-й цикл - конец'
-	else:
-		print '2-й цикл - начало'
-		start_key = line.find('[')
-		end_key = line.find(']') + 1
-		key = line[start_key:end_key]
-		my_example_dict[key] = line
-		print key
-		pretty(my_example_dict)
-		print 'Ключ найден. Начинаю новый элемент'
-		print line
-		print '2-й цикл - конец'
+>>> def cut_key_to_dict():
+	for line in f:
+		# find – находит подстроку в строке – возвращает позицию вхождения строки, либо -1:
+		if line.find('[') == -1:
+			print '1-й цикл - начало'
+			print 'Ключ не найден. Добавляю в словарь предыдущего элемента'
+			print line
+			print '1-й цикл - конец'
+		else:
+			print '2-й цикл - начало'
+			start_key = line.find('[')
+			end_key = line.find(']') + 1
+			key = line[start_key:end_key]
+			my_example_dict[key] = line
+			print key
+			pretty(my_example_dict)
+			print 'Ключ найден. Начинаю новый элемент'
+			print line
+			print '2-й цикл - конец'
+
+
 		
