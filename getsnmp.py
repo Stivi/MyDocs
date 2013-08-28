5458daf96dbbf23fbc122129881aba1ebd5ac430
sstev3n <sstev3n@gmail.com>
2013-08-28 09:01:39 +0400

    Лишний код

--- getsnmp.py
+++ getsnmp.py
@@ -3,17 +3,6 @@ import os
 import sys
 import subprocess
 
-# открывает utf-8 строку как cp1251
-
-s = "РњР°Р»Р°СЏ РјРѕС‰РЅРѕСЃС‚СЊ"
-u = unicode(s, "utf-8", errors='ignore')
-print u
-Малая мощность
-
-keys = u'-v 1 -c public MSK-VPS-MFP 1.3.6.1.4.1.1347.43.18.2.1.2.1.1'
-cmd = u'C:\\Users\\ssv\\Documents\\Programs\\net-snmp\\usr\\bin\\snmpget.exe'
-cmd = cmd + ' ' + keys
-PIPE = subprocess.PIPE
 
 def give_status_print():
     keys = u'-v 1 -c public MSK-VPS-MFP 1.3.6.1.4.1.1347.43.18.2.1.2.1.1'
