e4b58f5b0b2cf34476170ed79717270d22f5fc45
sstev3n <sstev3n@gmail.com>
2013-08-28 09:53:00 +0400

    Удаляем лишние пробелы

--- getsnmp.py
+++ getsnmp.py
@@ -6,7 +6,7 @@ import subprocess
 
 def give_status_print(devicename):
     args = [u'C:\\Users\\ssv\\Documents\\Programs\\net-snmp\\usr\\bin\\snmpget.exe', u'-O Tav -v 1 -c public', u'1.3.6.1.4.1.1347.43.18.2.1.2.1.1']
-    args.insert(2, devicename)
+    args.insert(2, devicename.strip())
     cmd = ' '.join(args)
     PIPE = subprocess.PIPE
     p = subprocess.Popen(cmd, shell=True, stdin=PIPE, stdout=PIPE, stderr=subprocess.STDOUT)
