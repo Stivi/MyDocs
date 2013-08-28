04851aef9c09098fac3d497bf62d73d196e79e5a
sstev3n <sstev3n@gmail.com>
2013-08-28 13:25:43 +0400

    вход функции в юникод

--- getsnmp.py
+++ getsnmp.py
@@ -6,7 +6,7 @@ import subprocess
 
 def give_status_print(devicename):
     args = [u'C:\\Users\\ssv\\Documents\\Programs\\net-snmp\\usr\\bin\\snmpget.exe', u'-O Tav -v 1 -c public', u'1.3.6.1.4.1.1347.43.18.2.1.2.1.1']
-    args.insert(2, devicename.strip())
+    args.insert(2, unicode(devicename.strip(), 'cp1251'))
     cmd = ' '.join(args)
     PIPE = subprocess.PIPE
     p = subprocess.Popen(cmd, shell=True, stdin=PIPE, stdout=PIPE, stderr=subprocess.STDOUT)
