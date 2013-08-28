f1625ba81a14bc719490dfb48b010a86a5418ec0
sstev3n <sstev3n@gmail.com>
2013-08-28 08:55:05 +0400

    function get value

---
+++ getsnmp.py
@@ -0,0 +1,38 @@
+import codecs
+import os
+import sys
+import subprocess
+
+# открывает utf-8 строку как cp1251
+
+s = "РњР°Р»Р°СЏ РјРѕС‰РЅРѕСЃС‚СЊ"
+u = unicode(s, "utf-8", errors='ignore')
+print u
+Малая мощность
+
+keys = u'-v 1 -c public MSK-VPS-MFP 1.3.6.1.4.1.1347.43.18.2.1.2.1.1'
+cmd = u'C:\\Users\\ssv\\Documents\\Programs\\net-snmp\\usr\\bin\\snmpget.exe'
+cmd = cmd + ' ' + keys
+PIPE = subprocess.PIPE
+
+# TODO function(имя машины)
+def give_status_print():
+    p = subprocess.Popen(cmd, shell=True, stdin=PIPE, stdout=PIPE, stderr=subprocess.STDOUT)
+    while True:
+        s = p.stdout.read()
+        u = unicode(s, 'utf-8', errors='ignore')
+        if not s:
+            break
+        print u,
+
+def new_func(mystring):
+	snmpbin = [u'dir', '/Q', '/N']
+	snmpbin.insert(1, mystring)
+	p = subprocess.Popen(snmpbin, shell=True, stdin=PIPE, stdout=PIPE, stderr=subprocess.STDOUT)
+	while True:
+		s = p.stdout.read()
+		if not s:
+			break
+		print s,
+
+
