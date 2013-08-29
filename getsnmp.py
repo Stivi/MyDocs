5b94d2d7ba4b171dee4b7e98beb0d02c25b2b5eb
sstev3n <sstev3n@gmail.com>
2013-08-29 09:01:38 +0400

    Удобочитаемость кода

--- getsnmp.py
+++ getsnmp.py
@@ -1,12 +1,11 @@
-import codecs
-import os
-import sys
 import subprocess
 
 
 def kyocera_status_print(devicename):
     args = [u'C:\\Users\\ssv\\Documents\\Programs\\net-snmp\\usr\\bin\\snmpget.exe', u'-O Tav -v 1 -c public', u'1.3.6.1.4.1.1347.43.18.2.1.2.1.1']
-    args.insert(2, unicode(devicename.strip(), 'cp1251'))
+    devicename = devicename.strip()
+    devicename = unicode(devicename, 'cp1251')
+    args.insert(2, devicename)
     cmd = ' '.join(args)
     PIPE = subprocess.PIPE
     p = subprocess.Popen(cmd, shell=True, stdin=PIPE, stdout=PIPE, stderr=subprocess.STDOUT)
