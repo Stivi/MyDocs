1bef351a2e979483f0f166cfeb8e8d3df93faa42
Stivi <sstev3n@gmail.com>
2013-10-06 13:04:53 +0400

    all to ogg

---
+++ flac_ape_m4a_wv_to-ogg.bat
@@ -0,0 +1,17 @@
+@ECHO OFF
+
+set flac="c:\Program Files (x86)\Exact Audio Copy\Flac\flac.exe"
+set oggenc="c:\Users\Sergey\Documents\Programs\oggenc2.87-1.3.3-x64\oggenc2.exe"
+set mac="c:\Program Files\Monkey's Audio\MAC.exe"
+set mplayer="c:\Program Files\mplayer\mplayer.exe"
+
+CLS
+IF EXIST *.pk DEL *.pk
+
+FOR %%I IN (*.flac) DO (%flac% -d -F "%%I" && DEL "%%I")
+
+FOR %%I IN (*.ape) DO (%mac% "%%I" "%%I.wav" -d && DEL "%%I")
+
+FOR %%I IN (*.m4a,*.wv) DO (%mplayer% -vc null -vo null -ao pcm:waveheader:file="%%I".wav "%%I" && DEL "%%I")
+
+FOR %%I IN (*.wav) DO (%oggenc% --quality 5 "%%I" && DEL "%%I")
