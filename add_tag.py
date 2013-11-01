169e6e22ae1d9f3077826d74fa99bf413ce5f051
Stivi <sstev3n@gmail.com>
2013-11-01 18:53:14 +0400

    It's work

--- add_tag.py
+++ add_tag.py
@@ -1,6 +1,16 @@
-def add_tag(input_string):
-    return '\Bla-bla{' + input_string + '}'
-
-def process_me(my_text):
-    search = 'чтобы'
-    return my_text.replace(search, add_tag(search))
+def process_func():
+    out_text = codecs.open("outme", "wb", "utf-8")
+    f = codecs.open("text.tex", "r", "utf-8")
+    f.seek(0)
+    p = re.compile(r"([Чч]то|what|lol)", re.UNICODE)
+    while True:
+        line = f.readline()
+        if not line:
+            print ('выхожу из цикла')
+            out_text.close()
+            f.close()
+            break
+        else:
+            print (p.search(line))
+            subline = p.sub(r'\Blabla{\g<1>}', line)
+            out_text.write(subline)
