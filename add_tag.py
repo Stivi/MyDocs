d18275d3c49973d6444b6b8382bddd985928b4f8
Stivi <sstev3n@gmail.com>
2013-11-09 13:49:20 +0400

    Use re.VERBOSE to understand regexp

--- add_tag.py
+++ add_tag.py
@@ -9,9 +9,39 @@ def process_func():
     out_text = codecs.open("outme", "wb", "utf-8")
     f = codecs.open("text.tex", "r", "utf-8")
     f.seek(0)
-    p = re.compile(
-        r"(\b[Аа]\b|\b[Бб]удто\b|\b[Дд]а\b|\b[Дд]ля того\b|\b[Ее]два\b|\b[Ее]сли\b|\b[Зз]ато\b|\b[Ии]\b|\b[Ии]ли\b|\b[Кк]ак\b|\b[Кк]огда\b|\b[Лл]ибо\b|\b[Нн]е то\b|\b[Нн]е только\b|\b[Нн]и\b|\b[Нн]о\b|\b[Оо]днако\b|\b[Оо]ттого\b|\b[Пп]отому что\b|\b[Пп]оэтому\b|\b[Сс]ловно\b|\b[Тт]ак\b|\b[Тт]акже\b|\b[Тт]аким образом\b|\b[Тт]о\b|\b[Тт]оже\b|\b[Чч]то\b|\b[Чч]тобы\b)",
-        re.UNICODE)
+    p = re.compile(r"""
+        (
+            \b[Аа]\b
+            |\b[Бб]удто\b
+            |\b[Дд]а\b
+            |\b[Дд]ля\ того\b       # In verbose regular expression
+            |\b[Ее]два\b            # whitespace is ignored. So I
+            |\b[Ее]сли\b            # putting a backslash
+            |\b[Зз]ато\b            # in front of space to match it.
+            |\b[Ии]\b
+            |\b[Ии]ли\b
+            |\b[Кк]ак\b
+            |\b[Кк]огда\b
+            |\b[Лл]ибо\b
+            |\b[Нн]е\ то\b
+            |\b[Нн]е\ только\b
+            |\b[Нн]и\b
+            |\b[Нн]о\b
+            |\b[Оо]днако\b
+            |\b[Оо]ттого\b
+            |\b[Пп]отому\ что\b
+            |\b[Пп]оэтому\b
+            |\b[Сс]ловно\b
+            |\b[Тт]ак\b
+            |\b[Тт]акже\b
+            |\b[Тт]аким\ образом\b
+            |\b[Тт]о\b
+            |\b[Тт]оже\b
+            |\b[Чч]то\b
+            |\b[Чч]тобы\b
+        )
+        """, re.VERBOSE)
+
     while True:
         line = f.readline()
         if not line:
