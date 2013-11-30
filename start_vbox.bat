eb0e2da93e37c224463ee159e910113e2d118436
Stivi <sstev3n@gmail.com>
2013-11-30 23:13:17 +0400

    use variable

--- start_vbox.bat
+++ start_vbox.bat
@@ -6,7 +6,7 @@ SET PUTTY="c:\Users\Sergey\Documents\Programs\Putty\PUTTY.EXE"
 SET VBoxManage="c:\Program Files\Oracle\VirtualBox\VBoxManage.exe"
 
 %VBoxManage% startvm sid
-c:\Users\Sergey\Documents\Programs\Putty\PUTTY.EXE -load "Debian" -l user -pw 99
+%PUTTY% -load "Debian" -l user -pw 99
 %VBoxManage% controlvm sid savestate
 
 COPY /Y d:\Stev\share\ideas\triz\genius\genius.pdf d:\Shared_books\Dropbox\genius.pdf
