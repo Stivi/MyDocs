cf97c896f082238b3c8e38c4c5d845068c04c3b8
Stivi <sstev3n@gmail.com>
2013-11-30 19:54:34 +0400

    delete unnecessary code

--- start_vbox.bat
+++ start_vbox.bat
@@ -5,16 +5,8 @@ CLS
 SET PUTTY="c:\Users\Sergey\Documents\Programs\Putty\PUTTY.EXE"
 SET VBoxManage="c:\Program Files\Oracle\VirtualBox\VBoxManage.exe"
 
-REM %VBoxManage% list vms
-
-REM %VBoxManage% > 1.txt
-
-
 %VBoxManage% startvm sid
 c:\Users\Sergey\Documents\Programs\Putty\PUTTY.EXE -load "Debian" -l user -pw 99
 %VBoxManage% controlvm sid savestate
 
 COPY /Y d:\Stev\share\ideas\triz\genius\genius.pdf d:\Shared_books\Dropbox\genius.pdf
-
-REM pause
-
