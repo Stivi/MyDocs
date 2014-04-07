a3b00362f86c4401a8f4ad918f28b8726ad0bd59
sstev3n <sstev3n@gmail.com>
2014-04-07 13:38:44 +0400

    Add pretty func

--- convert_to_ipynb/convert_to_ipynb.py
+++ convert_to_ipynb/convert_to_ipynb.py
@@ -1,19 +1,26 @@
+# -*- coding: utf-8 -*-
+
 import codecs
 
-out_text = codecs.open("outme", "wb", "utf-8")
 input_text = codecs.open("maintext", "r", "utf-8")
 input_text.seek(0)
 
 big_lol_test = []
 
+
+def pretty(input_unicode_escape_obj):
+     print repr(input_unicode_escape_obj).decode("unicode-escape")
+
+
 def grab_text(enter_text):
-    enter_text.seek(0)
     while True:
         somestring = enter_text.readline()
         if not somestring:
             print 'end of work'
             break
         if somestring.isspace():
-            print 'пустая строка. current list append to full list, and clear. Maybe pass here'
+            print 'null line. current list append to full list, and clear. Maybe pass here'
         else:
             big_lol_test.append(somestring)
+
+grab_text(input_text)
