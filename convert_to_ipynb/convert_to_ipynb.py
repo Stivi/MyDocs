f9f65d1175ba0928e39dcbd230ec1aa121ed6ab5
sstev3n <sstev3n@gmail.com>
2014-04-07 13:28:10 +0400

    new function to grab text

--- convert_to_ipynb/convert_to_ipynb.py
+++ convert_to_ipynb/convert_to_ipynb.py
@@ -4,11 +4,16 @@ out_text = codecs.open("outme", "wb", "utf-8")
 input_text = codecs.open("maintext", "r", "utf-8")
 input_text.seek(0)
 
-while True:
-    line = input_text.readline()
-    if not line:
-        out_text.close()
-        input_text.close()
-        break
-    else:
-        out_text.write(line.encode('unicode_escape'))
+big_lol_test = []
+
+def grab_text(enter_text):
+    enter_text.seek(0)
+    while True:
+        somestring = enter_text.readline()
+        if not somestring:
+            print 'end of work'
+            break
+        if somestring.isspace():
+            print 'пустая строка. current list append to full list, and clear. Maybe pass here'
+        else:
+            big_lol_test.append(somestring)
