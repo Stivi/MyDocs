d08a9254f3b0390a04fb8e043179df91a9e21d20
sstev3n <sstev3n@gmail.com>
2014-04-08 09:59:54 +0400

    Основа функции

--- convert_to_ipynb/convert_to_ipynb.py
+++ convert_to_ipynb/convert_to_ipynb.py
@@ -5,11 +5,23 @@ import codecs
 input_text = codecs.open("maintext", "r", "utf-8")
 input_text.seek(0)
 
-big_lol_test = []
+sample_list = []
 
 
 def pretty(input_unicode_escape_obj):
      print repr(input_unicode_escape_obj).decode("unicode-escape")
+
+# если '1' не существует, то #1, иначе #2
+#1
+sample_list.append({'1': ['first']})
+#2
+sample_list[len(sample_list)]['1'].append('really cool')
+
+sample_list.append({'2': ['second']})
+sample_list[1]['2'].append('really cool')
+
+pretty(sample_list)
+print len(sample_list)
 
 
 # прототип ф-ции
@@ -26,6 +38,6 @@ def grab_text(enter_text):
             print 'func_isspace()' + str(counter)
         else:
             print 'func_add_paragraph()' + str(counter)
-            #big_lol_test.append(somestring)
+
 
 grab_text(input_text)
