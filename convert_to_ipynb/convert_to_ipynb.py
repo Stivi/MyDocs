24be88ae260630c2c911fc965d91e71eff305841
sstev3n <sstev3n@gmail.com>
2014-04-08 12:15:17 +0400

    Refactoring

--- convert_to_ipynb/convert_to_ipynb.py
+++ convert_to_ipynb/convert_to_ipynb.py
@@ -3,7 +3,6 @@
 import codecs
 
 input_text = codecs.open("maintext", "r", "utf-8")
-input_text.seek(0)
 
 sample_list = [{1: []}]
 
@@ -13,9 +12,10 @@ def pretty(input_unicode_escape_obj):
 
 
 def func_add_paragraph(num_string, input_string):
-    print 'index_is ' + str(len(sample_list)-1)
-    if num_string in sample_list[len(sample_list)-1]:
-        sample_list[len(sample_list)-1][num_string].append(input_string)
+    current_index = len(sample_list) - 1
+    print 'index_is ' + str(current_index)
+    if num_string in sample_list[current_index]:
+        sample_list[current_index][num_string].append(input_string)
     else:
         sample_list.append({num_string: [input_string]})
 
@@ -27,13 +27,12 @@ def grab_text(enter_text):
     while True:
         somestring = enter_text.readline()
         if not somestring:
-            print 'func_write_obj_with_paragraphs()'
+            print 'func_add_cells_to_worksheet()'
             break
         if somestring.isspace():
             counter += 1
             print 'func_isspace()' + str(counter)
         else:
-            #print 'func_add_paragraph()' + str(counter)
             func_add_paragraph(counter, somestring)
 
 
