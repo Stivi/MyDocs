ad3691e8bbd1ad119cc3c1b19d9ffe97ade91f74
sstev3n <sstev3n@gmail.com>
2014-04-08 11:11:04 +0400

    func_for_paragraph is work

--- convert_to_ipynb/convert_to_ipynb.py
+++ convert_to_ipynb/convert_to_ipynb.py
@@ -5,23 +5,19 @@ import codecs
 input_text = codecs.open("maintext", "r", "utf-8")
 input_text.seek(0)
 
-sample_list = []
+sample_list = [{1: []}]
 
 
 def pretty(input_unicode_escape_obj):
      print repr(input_unicode_escape_obj).decode("unicode-escape")
 
-# если '1' не существует, то #1, иначе #2
-#1
-sample_list.append({'1': ['first']})
-#2
-sample_list[len(sample_list)]['1'].append('really cool')
 
-sample_list.append({'2': ['second']})
-sample_list[1]['2'].append('really cool')
-
-pretty(sample_list)
-print len(sample_list)
+def func_add_paragraph(num_string, input_string):
+    print 'index_is ' + str(len(sample_list)-1)
+    if num_string in sample_list[len(sample_list)-1]:
+        sample_list[len(sample_list)-1][num_string].append(input_string)
+    else:
+        sample_list.append({num_string: [input_string]})
 
 
 # прототип ф-ции
@@ -37,7 +33,9 @@ def grab_text(enter_text):
             counter += 1
             print 'func_isspace()' + str(counter)
         else:
-            print 'func_add_paragraph()' + str(counter)
+            #print 'func_add_paragraph()' + str(counter)
+            func_add_paragraph(counter, somestring)
 
 
 grab_text(input_text)
+pretty(sample_list)
