797511c3410fc6369f158fc59cb7c79c82faa8a9
sstev3n <sstev3n@gmail.com>
2014-05-13 14:58:11 +0400

    Получить пользователей, добавить в список

--- jabber/delete_jabber_user.py
+++ jabber/delete_jabber_user.py
@@ -1,6 +1,29 @@
 # -*- coding: utf-8 -*-
 
+import codecs
+
+f = codecs.open("users.txt", "r", "utf-8")
+
+
 def list_users():
     print('ejabberdctl registered-users localhost')
 
+
+def pretty(dict):
+     print repr(dict).decode("unicode-escape")
+
+
 list_users()
+
+users_list = []
+
+while True:
+    line = f.readline()
+    if not line:
+        print('выхожу из цикла')
+        f.close()
+        break
+    else:
+        users_list.append(line)
+
+pretty(users_list)
