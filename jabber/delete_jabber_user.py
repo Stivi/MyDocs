95326ba00c9b9c62b09ece31f74078e5b5fdb542
sstev3n <sstev3n@gmail.com>
2014-05-14 16:55:14 +0400

    Release

--- jabber/delete_jabber_user.py
+++ jabber/delete_jabber_user.py
@@ -4,6 +4,8 @@ import codecs
 import subprocess
 
 
+subprocess.call('ejabberdctl registered-users localhost > users.txt', shell=True)
+
 f = codecs.open("users.txt", "r", "utf-8")
 
 users_list = []
@@ -12,15 +14,16 @@ while True:
     line = f.readline()
     if not line:
         f.close()
-        print('выхожу из цикла')
+        print('Загружен список пользователей.')
         break
     else:
         users_list.append(line)
 
 
-delete_user = "timkina_irina_vladimirovna"
+delete_user = "belyaev_aleksey_evgenievich"
+
 
 for user in users_list:
     commands = " ejabberdctl delete-rosteritem " + user.strip() + " localhost " + delete_user.strip() + " localhost"
     subprocess.call(commands, shell=True)
-    print(' done for user: ' + user.strip())
+    print('Done for user: ' + user.strip())
