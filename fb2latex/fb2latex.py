ca46ab23f2b25b1fd35ad37a52264d7ca36535a3
Stivi <sstev3n@gmail.com>
2014-07-07 21:39:43 +0400

    Delete fucking comments

--- fb2latex/fb2latex.py
+++ fb2latex/fb2latex.py
@@ -9,25 +9,6 @@ root = tree.getroot()
 
 # https://docs.python.org/3/library/xml.etree.elementtree.html
 
-# Element  is  a  flexible   container  object  designed  to
-# store hierarchical  data structures  in memory. It  can be
-# described as a cross between a list and a dictionary. Each
-# Element has a number of properties associated with it:
-#
-#    'tag' - a string containing the element's name.
-#
-#    'attributes'   -  a   Python  dictionary   storing  the
-#    element's attributes.
-#
-#    'text'  -  a  string   containing  the  element's  text
-#    content.
-#
-#    'tail' -  an optional string containing  text after the
-#    element's end tag.
-#
-#    And  a number  of  child elements  stored  in a  Python
-#    sequence.
-
 
 # As an Element, root has a tag and a dictionary of attributes
 root.tag
