be3004aeb13c2058ad176404e04b3ab044393766
sstev3n <sstev3n@gmail.com>
2014-07-16 10:01:19 +0400

    Refactor

--- all2one/all2one.sh
+++ all2one/all2one.sh
@@ -1,18 +1,15 @@
 #!/bin/bash
 
-
 push_to_file() {
     cat $1 >> out.log
 }
 
-
 work() {
     counter=52
-	until [ $counter -lt 1 ]; do
-		push_to_file access.log.$counter
-		let counter-=1
-	done
+    until [ $counter -lt 1 ]; do
+        push_to_file access.log.$counter
+        let counter-=1
+    done
 }
-
 
 work
