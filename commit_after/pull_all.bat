7875c03762839d7d0d159e136f577f834e0c6e3a
sstev3n <sstev3n@gmail.com>
2014-09-08 17:15:23 +0400

    08.09.2014 17:15:23,28

--- commit_after/pull_all.bat
+++ commit_after/pull_all.bat
@@ -11,40 +11,32 @@ set bucket_books="%HOMEDRIVE%%HOMEPATH%\bucket\books"
 set bucket_ledger="%HOMEDRIVE%%HOMEPATH%\bucket\ledger"
 
 echo ---------->> c:\log
-echo push_start:>> c:\log
+echo pull_start:>> c:\log
 echo %DATE%>> c:\log
 echo %TIME%>> c:\log
 
-
 CD /D %git_vim%
-call git commit -a -m "%DATE% %TIME%"
-call git push
+call git pull
 
 CD /D %git_books%
-call git commit -a -m "%DATE% %TIME%"
-call git push
+call git pull
 
 CD /D %git_multimedia%
-call git commit -a -m "%DATE% %TIME%"
-call git push
+call git pull
 
 CD /D %git_python%
-call git commit -a -m "%DATE% %TIME%"
-call git push
+call git pull
 
 CD /D %git_scripts%
-call git commit -a -m "%DATE% %TIME%"
-call git push
+call git pull
 
 CD /D %bucket_books%
-call git commit -a -m "%DATE% %TIME%"
-call git push
+call git pull
 
 CD /D %bucket_ledger%
-call git commit -a -m "%DATE% %TIME%"
-call git push
+call git pull
 
 echo ---------->> c:\log
-echo push_stop:>> c:\log
+echo pull_end:>> c:\log
 echo %DATE%>> c:\log
 echo %TIME%>> c:\log
