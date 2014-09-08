4d6d7032cd128acf63aa06d086278bb384110127
sstev3n <sstev3n@gmail.com>
2014-09-08 15:34:16 +0400

    08.09.2014 15:34:16,75

--- start_vbox.bat
+++ start_vbox.bat
@@ -5,8 +5,10 @@ CLS
 SET PUTTY="c:\Users\Sergey\Documents\Programs\Putty\PUTTY.EXE"
 SET VBoxManage="c:\Program Files\Oracle\VirtualBox\VBoxManage.exe"
 
+
 %VBoxManage% startvm sid
 %PUTTY% -load "Debian" -l user -pw 99
 %VBoxManage% controlvm sid savestate
+
 
 COPY /Y d:\Stev\share\ideas\triz\genius\genius.pdf d:\Shared_books\Dropbox\genius.pdf
